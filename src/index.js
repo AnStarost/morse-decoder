c777043a79fcafb42c19860cae6c6fdd4b10fbb5
AnStarost <anstarost.mail@gmail.com>
2022-08-03 16:28:18 +0300

    feat: Morse decode

--- src/index.js
+++ src/index.js
@@ -38,8 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    // var decoded = expr.split('   ').map((elem) => elem.split(' ')).map(letter => MORSE_TABLE[letter]);
+    // return typeof(expr);
+
+    let size = 10; //размер подмассива
+    let letters = []; //массив в который будет выведен результат.
+    for (let i = 0; i < expr.length/size; i++){
+        letters[i] = expr.slice((i*size), (i*size) + size);
+    }
+
+    const newLetter = letters.map((letter) => {
+        let x = false;
+        if (letter === '**********') {
+            return ' ';
+        }
+        const noZero = letter.split('').filter((digit) => {
+            if (digit === '1') {
+                x = true            
+            }
+            return x 
+        }).join('')
+
+        let size = 2; //размер подмассива
+        let noZero2sign = []; //массив в который будет выведен результат.
+        for (let i = 0; i < noZero.length/size; i++) {
+            noZero2sign[i] = noZero.slice((i*size), (i*size) + size);
+        }
+    
+        const coverted = noZero2sign.map((e) => {
+            if (e === '10') {
+                return '.'
+            } else 
+                return '-'
+        }).join('');
+        return MORSE_TABLE[coverted];
+    });
+
+    return newLetter.join('');
+
+}    
+    
 
 module.exports = {
     decode
